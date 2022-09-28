71963d2960e725a32fb68188d93b344d9fa353e7
Pfeilmayer, Robert <Robert.Pfeilmayer@tecalliance.net>
2022-09-28 10:44:11 +0200

    Linq exercises 1-5

--- Linq_Aufgaben/Linq_Aufgaben/Program.cs
+++ Linq_Aufgaben/Linq_Aufgaben/Program.cs
@@ -10,11 +10,45 @@ namespace Linq_Aufgaben
     {
         static void Main(string[] args)
         {
+            //Aufgabe 5
+
+            Console.Write("\nLINQ : Display the characters and frequency of character from giving string : ");
+            Console.Write("\n----------------------------------------------------------------------------\n");
+            Console.Write("Input the string : ");
+            string str = Console.ReadLine();
+            Console.Write("\n");
+
+            var FreQ = from x in str
+                       group x by x into y
+                       select y;
+            Console.Write("The frequency of the characters are :\n");
+            foreach (var ArrEle in FreQ)
+            {
+                Console.WriteLine("Character " + ArrEle.Key + ": " + ArrEle.Count() + " times");
+            }
+            Console.ReadLine();
+
             //Aufgabe 4
-            
-            
+            /*
+            int[] arr1 = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var n = from x in arr1
+                    group x by x into y
+                    select y;
+
+            Console.WriteLine("\nThe number and the Frequency are : \n");
+            foreach (var arrNo in n)
+            {
+                Console.WriteLine("Number " + arrNo.Key + " appears " + arrNo.Count() + " times");
+            }
+
+            Console.ReadLine();
+
+            */
+
             //Aufgabe 3
 
+            /*
             var myArray = new[] { 3, 9, 2, 8, 6, 5 };
 
             Console.Write("\nLINQ : Find the number and its square of an array which is more than 20 : ");
@@ -30,45 +64,50 @@ namespace Linq_Aufgaben
                 Console.WriteLine(a);
 
             Console.ReadLine();
+            */
+
 
             //Aufgabe 2
+
             /*
-            List<int> ints1 = new List<int>();
+            List<int> ints = new List<int>();
             for (int i = 1; i < 101; i++)
             {
-                ints1.Add(i);
+                ints.Add(i);
             }
 
-            foreach (int i in ints1)
+            var result = from s in ints
+                         where s % 2 != 0
+                         select s;
+
+            foreach (int i in result)
             {
-                if (i % 2 != 0)
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine(i);
             }
-            
+            Console.ReadLine();
+            */
+
+            //Aufgabe 1
+
+            /*
+            List<int> ints = new List<int>();
+            for (int i = 1; i < 101; i++)
+            {
+                ints.Add(i);
+            }
+
+            var result = from s in ints
+                         where s % 2 == 0
+                         select s;
+
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadLine();
             */
 
 
-            //Aufgabe 1
-            /*
-                        List<int> ints = new List<int>();
-                        for (int i = 1; i < 101; i++)
-                        {
-                            ints.Add(i);
-                        }
-
-                        foreach (int i in ints)
-                        {
-                            if (i % 2 == 0)
-                            {
-                                Console.WriteLine(i);
-                            }
-                        }
-
-                        Console.ReadLine();
-            */
         }
     }
 }
