ae36f8e67d8c20efd13e0a002c8346c423392bbf
Pfeilmayer, Robert <Robert.Pfeilmayer@tecalliance.net>
2022-09-28 09:41:56 +0200

    Aufgaben 1 & 2 Linq

---
+++ Linq_Aufgaben/Linq_Aufgaben/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Aufgaben
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Aufgabe 2
+
+            List<int> ints1 = new List<int>();
+            for (int i = 1; i < 101; i++)
+            {
+                ints1.Add(i);
+            }
+
+            foreach (int i in ints1)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+
+            //Aufgabe 1
+
+            List<int> ints = new List<int>();
+            for (int i = 1; i < 101; i++)
+            {
+                ints.Add(i);
+            }
+
+            foreach (int i in ints)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
